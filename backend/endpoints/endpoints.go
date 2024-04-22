3f77456ddc570adc4fb231b3f1d8a39ac0ae2848
Danielis <Sarpalius>
2024-04-23 00:26:15 +0300

    Modified the passwords data that has to be sent

--- backend/endpoints/endpoints.go
+++ backend/endpoints/endpoints.go
@@ -24,18 +24,27 @@ func CreatePassword(c *gin.Context) {
 	// Logic to create a new password
 }
 
-func getPasswords(db *sql.DB, userID int) ([]string, error) {
-	var passwords []string
+type Password struct {
+	ID       int
+	UserID   int
+	Password string
+	Username string
+	Website  string
+	Name     string
+}
 
-	rows, err := db.Query("SELECT password FROM passwords WHERE user_id = ?", userID)
+func getPasswords(db *sql.DB, userID int) ([]Password, error) {
+	var passwords []Password
+
+	rows, err := db.Query("SELECT id, user_id, password, username, website, name FROM saved_passwords WHERE user_id = ?", userID)
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
 
 	for rows.Next() {
-		var password string
-		if err := rows.Scan(&password); err != nil {
+		var password Password
+		if err := rows.Scan(&password.ID, &password.UserID, &password.Password, &password.Username, &password.Website, &password.Name); err != nil {
 			return nil, err
 		}
 		passwords = append(passwords, password)
@@ -73,20 +82,57 @@ func LoginUser(db *sql.DB) gin.HandlerFunc {
 				// c.JSON(http.StatusOK, gin.H{"token": token})
 
 				// Fetch and decrypt the user's passwords
-				passwords, err := getPasswords(db, user.Id) // This needs implementation
+				passwords, err := getPasswords(db, user.Id)
 				if err != nil {
 					log.Println("Failed to fetch passwords")
 					return
 				}
 
-				decryptedPasswords := make(map[string]string)
+				decryptedPasswords := make([]Password, 0, len(passwords))
 				for _, password := range passwords {
-					decryptedPassword, err := userManagement.Decrypt([]byte(password), []byte(user.DecryptionKey))
+					decryptedPassword := Password{
+						ID:       password.ID,
+						UserID:   password.UserID,
+						Password: "",
+						Username: "",
+						Website:  "",
+						Name:     "",
+					}
+
+					var decryptedPasswordBytes []byte
+					var err error
+
+					decryptedPasswordBytes, err = userManagement.Decrypt([]byte(password.Password), []byte(user.DecryptionKey))
 					if err != nil {
 						log.Println("Failed to decrypt password")
 						return
 					}
-					decryptedPasswords[password] = string(decryptedPassword)
+					decryptedPassword.Password = string(decryptedPasswordBytes)
+
+					decryptedPasswordBytes, err = userManagement.Decrypt([]byte(password.Username), []byte(user.DecryptionKey))
+					if err != nil {
+						log.Println("Failed to decrypt username")
+						return
+					}
+					decryptedPassword.Username = string(decryptedPasswordBytes)
+
+					decryptedPasswordBytes, err = userManagement.Decrypt([]byte(password.Website), []byte(user.DecryptionKey))
+					if err != nil {
+						log.Println("Failed to decrypt website")
+						return
+					}
+
+					decryptedPassword.Website = string(decryptedPasswordBytes)
+
+					decryptedPasswordBytes, err = userManagement.Decrypt([]byte(password.Name), []byte(user.DecryptionKey))
+					if err != nil {
+						log.Println("Failed to decrypt name")
+						return
+					}
+
+					decryptedPassword.Name = string(decryptedPasswordBytes)
+
+					decryptedPasswords = append(decryptedPasswords, decryptedPassword)
 				}
 
 				// Send the decrypted passwords to the client
